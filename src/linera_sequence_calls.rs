c0492b3c4c4e240181437d426d7144915338243d
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-10-24 14:21:09 +0200

    extend the functionalities.

--- src/linera_sequence_calls.rs
+++ src/linera_sequence_calls.rs
@@ -3,7 +3,7 @@ extern crate serde;
 extern crate serde_json;
 extern crate sysinfo;
 mod common;
-use common::read_config_file;
+use common::{kill_processes, read_config_file};
 use serde::Deserialize;
 use std::fs::File;
 use std::path::Path;
@@ -11,16 +11,59 @@ use std::process::Command;
 
 #[derive(Deserialize)]
 struct Entry {
+    nature: String,
+    #[serde(default)]
     directory: String,
+    #[serde(default)]
     command: String,
+    #[serde(default)]
     stdout: String,
+    #[serde(default)]
     stderr: String,
+    #[serde(default)]
+    kill_names: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct Config(Vec<Entry>);
+
+
+fn execute_command(i_command: usize, entry: &Entry) -> anyhow::Result<()> {
+    let directory = &entry.directory;
+    let command = &entry.command;
+    let stdout = &entry.stdout;
+    let stderr = &entry.stderr;
+    println!(
+        "directory={} command={} stdout={} stderr={}",
+        directory, command, stdout, stderr
+    );
+    //
+    let file_out = File::create(stdout)?;
+    let file_err = File::create(stderr)?;
+    let l_str = command
+        .split(' ')
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>();
+    let the_command = &l_str[0];
+    let mut comm_args = Vec::new();
+    for i in 1..l_str.len() {
+        comm_args.push(l_str[i].clone());
+    }
+    println!("the_command={} comm_args={:?}", the_command, comm_args);
+    let path = Path::new(&directory);
+    let output = Command::new(the_command)
+        .current_dir(path)
+        .stdout::<File>(file_out)
+        .stderr::<File>(file_err)
+        .args(comm_args)
+        .output()?;
+    println!("i_command={} output={:?}", i_command, output);
+    Ok(())
 }
 
 
 
-#[derive(Deserialize)]
-struct Config(Vec<Entry>);
+
 
 fn main() -> anyhow::Result<()> {
     let arguments = std::env::args().into_iter().collect::<Vec<_>>();
@@ -36,35 +79,20 @@ fn main() -> anyhow::Result<()> {
     let n_command = config.0.len();
     println!("n_command={}", n_command);
     for (i_command, entry) in config.0.into_iter().enumerate() {
-        let directory = entry.directory;
-        let command = entry.command;
-        let stdout = entry.stdout;
-        let stderr = entry.stderr;
-        println!(
-            "i_command={} directory={} command={} stdout={} stderr={}",
-            i_command, directory, command, stdout, stderr
-        );
-        //
-        let file_out = File::create(stdout)?;
-        let file_err = File::create(stderr)?;
-        let l_str = command
-            .split(' ')
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>();
-        let the_command = &l_str[0];
-        let mut comm_args = Vec::new();
-        for i in 1..l_str.len() {
-            comm_args.push(l_str[i].clone());
+        let nature = entry.nature.clone();
+        println!("i_command={i_command} nature={nature}");
+
+        match nature.trim() {
+            "execute_command" => {
+                execute_command(i_command, &entry)?;
+            },
+            "kill_processes" => {
+                kill_processes(&entry.kill_names);
+            },
+            _ => {
+                anyhow::bail!("No matching entry for nature={nature}");
+            }
         }
-        println!("the_command={} comm_args={:?}", the_command, comm_args);
-        let path = Path::new(&directory);
-        let output = Command::new(the_command)
-            .current_dir(path)
-            .stdout::<File>(file_out)
-            .stderr::<File>(file_err)
-            .args(comm_args)
-            .output()?;
-        println!("i_command={} output={:?}", i_command, output);
     }
 
     Ok(())
